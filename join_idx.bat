57a0250f83a1808266ad0c5846daaf3dbfe010bc
Jgoodz <Goodyer@gmail.com>
2017-01-08 23:16:18 -0500

    Update join_idx.bat

--- join_idx.bat
+++ join_idx.bat
@@ -3,6 +3,6 @@ for %%* in (.) do set CurrDirName=%%~nx*
 FOR %%A IN (%Date:/=%) DO SET Today=%%A
 
 
-for %%f in (*_idx_*) do ( type "%%f" >> "EGLGENERIC_PRF_0_%CurrDirName%.psv"
+for %%f in (*_idx_*) do ( type "%%f" >> "%CurrDirName%.psv"
 ECHO %%f
 ) 
